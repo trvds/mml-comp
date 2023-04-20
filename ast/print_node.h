ed7542b1891911c8cacefa4af33de85a6ac31bcc
David Martins de Matos <david.m.matos@gmail.com>
2023-04-20 11:01:00 +0100

    First MML version.

---
+++ ast/print_node.h
@@ -0,0 +1,32 @@
+#ifndef __MML_AST_PRINT_NODE_H__
+#define __MML_AST_PRINT_NODE_H__
+
+#include <cdk/ast/expression_node.h>
+
+namespace mml {
+
+  /**
+   * Class for describing print nodes.
+   */
+  class print_node: public cdk::basic_node {
+    cdk::expression_node *_argument;
+
+  public:
+    inline print_node(int lineno, cdk::expression_node *argument) :
+        cdk::basic_node(lineno), _argument(argument) {
+    }
+
+  public:
+    inline cdk::expression_node *argument() {
+      return _argument;
+    }
+
+    void accept(basic_ast_visitor *sp, int level) {
+      sp->do_print_node(this, level);
+    }
+
+  };
+
+} // mml
+
+#endif

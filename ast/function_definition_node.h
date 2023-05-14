b450213ec11e2136eb5d2a54b1499ad5fe80530c
Tiago Vieira da Silva <tiagovsilva@tecnico.ulisboa.pt>
2023-05-14 17:12:22 +0100

    Add functional type to function_definition_node

--- ast/function_definition_node.h
+++ ast/function_definition_node.h
@@ -1,28 +1,41 @@
 #ifndef __MML_AST_FUNCTION_H__
 #define __MML_AST_FUNCTION_H__
 
-#include <cdk/ast/expression_node.h>
+#include <cdk/ast/typed_node.h>
 
 namespace mml {
 
-  class function_definition_node : public cdk::expression_node {
+  class function_definition_node : public cdk::typed_node {
     cdk::sequence_node *_arguments;
     mml::block_node *_block;
     std::shared_ptr<cdk::basic_type> _outputType;
     bool _isMain = false;
 
   public:
-    function_definition_node(int lineno, cdk::sequence_node *arguments,
-                             mml::block_node *block)
-        : cdk::expression_node(lineno), _arguments(arguments), _block(block),
-          _outputType(cdk::primitive_type::create(0, cdk::TYPE_VOID)) {}
+    // Constructor for main function
+    function_definition_node(int lineno, mml::block_node *block)
+        : cdk::typed_node(lineno), _block(block),
+          _outputType(cdk::primitive_type::create(0, cdk::TYPE_INT)) {
+      // set type to int
+      type(cdk::primitive_type::create(0, cdk::TYPE_INT));
+    }
 
     function_definition_node(int lineno,
-                             std::shared_ptr<cdk::basic_type> funType,
+                             std::shared_ptr<cdk::basic_type> outputType,
                              cdk::sequence_node *arguments,
                              mml::block_node *block)
-        : cdk::expression_node(lineno), _arguments(arguments), _block(block),
-          _outputType(funType) {}
+        : cdk::typed_node(lineno), _arguments(arguments), _block(block),
+          _outputType(outputType) {
+      // Iterate over the arguments variable and create a
+      // std::vector<std::shared_ptr<basic_type>> variable with the types of the
+      // arguments
+      std::vector<std::shared_ptr<cdk::basic_type>> types;
+      for (size_t ax = 0; ax < _arguments->size(); ax++) {
+        types.push_back(argument(ax)->type());
+      }
+      // set type to functional type
+      type(cdk::functional_type::create(types, outputType));
+    }
 
   public:
     cdk::sequence_node *arguments() { return _arguments; }
@@ -32,6 +45,7 @@ namespace mml {
     mml::block_node *block() { return _block; }
     std::shared_ptr<cdk::basic_type> outputType() { return _outputType; }
     bool isMain() { return _isMain; }
+    void setMain() { _isMain = true; }
 
     void accept(basic_ast_visitor *sp, int level) {
       sp->do_function_definition_node(this, level);

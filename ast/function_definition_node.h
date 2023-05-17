e9ef903ccbabebb542cb4f16c9c61f94e0e7cd84
Tiago Vieira da Silva <tiagovsilva@tecnico.ulisboa.pt>
2023-05-17 08:15:16 +0100

    Fix E0 errors

--- ast/function_definition_node.h
+++ ast/function_definition_node.h
@@ -1,31 +1,23 @@
 #ifndef __MML_AST_FUNCTION_H__
 #define __MML_AST_FUNCTION_H__
 
+#include <cdk/ast/expression_node.h>
 #include <cdk/ast/typed_node.h>
 
 namespace mml {
 
-  class function_definition_node : public cdk::typed_node {
+  class function_definition_node : public cdk::expression_node {
     cdk::sequence_node *_arguments;
     mml::block_node *_block;
-    std::shared_ptr<cdk::basic_type> _outputType;
     bool _isMain = false;
 
   public:
-    // Constructor for main function
-    function_definition_node(int lineno, mml::block_node *block)
-        : cdk::typed_node(lineno), _block(block),
-          _outputType(cdk::primitive_type::create(0, cdk::TYPE_INT)) {
-      type(cdk::functional_type::create(_outputType));
-      _isMain = true;
-    }
-
     function_definition_node(int lineno,
                              std::shared_ptr<cdk::basic_type> outputType,
                              cdk::sequence_node *arguments,
-                             mml::block_node *block)
-        : cdk::typed_node(lineno), _arguments(arguments), _block(block),
-          _outputType(outputType) {
+                             mml::block_node *block, bool isMain = false)
+        : cdk::expression_node(lineno), _arguments(arguments), _block(block),
+          _isMain(isMain) {
       std::vector<std::shared_ptr<cdk::basic_type>> types;
       for (size_t ax = 0; ax < _arguments->size(); ax++) {
         types.push_back(argument(ax)->type());
@@ -39,7 +31,6 @@ namespace mml {
       return dynamic_cast<cdk::typed_node *>(_arguments->node(ax));
     }
     mml::block_node *block() { return _block; }
-    std::shared_ptr<cdk::basic_type> outputType() { return _outputType; }
     bool isMain() { return _isMain; }
 
     void accept(basic_ast_visitor *sp, int level) {
